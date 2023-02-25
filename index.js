d842c90b57c25e0041bd301393a3ddf21b718fb8
CINO1902 <caleboruta.co@gmail.com>
2023-02-25 16:50:39 +0100

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();   
 const express = require("express");
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
@@ -7,7 +8,7 @@ const cors = require('cors');
 const app = express();
 
 
-mongoose.connect("mongodb+srv://new_db:newdb1902@cluster0.9ll3qel.mongodb.net/Foodie"
+mongoose.connect(`mongodb+srv://new_db:${process.env.mongopassword}@cluster0.9ll3qel.mongodb.net/Foodie`
 ).then(() => console.log("Db Connected")).catch(()=> console.log("Database error"));
 
 app.use(express.json());
@@ -15,7 +16,7 @@ app.use((req, res, next) => {
     res.header ("Access-Control-Allow-Origin", "*");
     res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, "
     );
     if (req.method === 'OPTIONS') {
         res.header ('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE,GET');
