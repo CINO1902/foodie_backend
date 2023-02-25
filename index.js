fc30bd229554dab7397e0becdfdb1fbb0a854e5d
CINO1902 <caleboruta.co@gmail.com>
2023-02-25 15:27:00 +0100

    first commit

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ mongoose.connect("mongodb+srv://new_db:newdb1902@cluster0.9ll3qel.mongodb.net/Fo
 ).then(() => console.log("Db Connected")).catch(()=> console.log("Database error"));
 
 app.use(express.json());
-// app.use((req, res, next) => {
-//     res.header ("Access-Control-Allow-Origin", "*");
-//     res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if (req.method === 'OPTIONS') {
-//         res.header ('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE,GET');
-//         return res.status(200).json({})
-//     }
-// });
+app.use((req, res, next) => {
+    res.header ("Access-Control-Allow-Origin", "*");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header ('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE,GET');
+        return res.status(200).json({})
+    }
+});
 app.use("/route",router);
 app.route("/").get((req,res)=>{
 res.json("hello world");
