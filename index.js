94024daca1113a404cc575a490786acc579ad2db
CINO1902 <caleboruta.co@gmail.com>
2023-02-25 18:25:46 +0100

    first commit

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const app = express();
 
 mongoose.connect("mongodb+srv://new_db:newdb1902@cluster0.9ll3qel.mongodb.net/Foodie"
 ).then(() => console.log("Db Connected")).catch(()=> console.log("Database error"));
-
+app.use(cors())
 app.use(express.json());
-app. use ((req, res, next) => {
-    res.header ("Access-Control-Allow-Origin", "*");
-    res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header ('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE,GET');
-        return res.status(200).json({})
-    }
-});
+// app.use((req, res, next) => {
+//     res.header ("Access-Control-Allow-Origin", "*");
+//     res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     if (req.method === 'OPTIONS') {
+//         res.header ('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE,GET');
+//         return res.status(200).json({})
+//     }
+// });
 app.use("/route",router);
 app.route("/").get((req,res)=>{
 res.json("hello world");
