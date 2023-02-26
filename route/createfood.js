931b5adf76c5d61e72f378fdb07cfeefeb0cb2e5
CINO1902 <caleboruta.co@gmail.com>
2023-02-26 18:46:20 +0100

    first commit

--- route/createfood.js
+++ route/createfood.js
@@ -580,7 +580,7 @@ async function getcart()  {
               limit : limit,
            }
        } 
-     console.log(pagnited)
+   
      res.json({status: 'success', result: pagnited}) 
     
     return result.reverse();
