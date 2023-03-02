03ebf497d793d5f4d4d44f175ed67203be90cb80
CINO1902 <caleboruta.co@gmail.com>
2023-03-02 11:09:58 +0100

    first commit

--- route/createfood.js
+++ route/createfood.js
@@ -1438,9 +1438,9 @@ let drink4amount = finddrinkamount4[0].amount
     console.log(foodforeach)
     console.log(drinkforeach)
     let outstandingfood = unused_money -percentage(30, unused_money)
-      let discount = (totalamount - percentage(30, totalamount)).toFixed(2)
+      let discount = (totalamount - percentage(30, totalamount)).round()
      let new_amount_to_pay = pricetopay - unused_money
-     let amount_to_pay = (new_amount_to_pay - percentage(30, new_amount_to_pay)).toFixed(2)
+     let amount_to_pay = (new_amount_to_pay - percentage(30, new_amount_to_pay)).round()
    if(checkorder.length != 0){
     let date = checkorder[checkorder.length -1].date
    // 
